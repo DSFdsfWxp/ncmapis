8de2f1e666b26e791c3d234fe455a32057900a55
Wxp <102459686+DSFdsfWxp@users.noreply.github.com>
2023-02-12 16:42:59 +0800

    last

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -2,13 +2,15 @@ export async function onRequest(context){
     var request = context.request;
     var worker = context.env.worker;
 
+    var ur = new URL(request.url);
     var o = {
         method: request.method,
         headers: request.headers,
         redirect: request.redirect,
         body: request.body
-      }
-    var r = new Request('https://ncmapis.dsfdsfwxp.workers.dev/' + context.params.catchall.join('/'),o);
+    };
+    var u = Array.isArray(context.params.catchall) ? (context.params.catchall.length!=0 ? context.params.catchall.join('/') : '/') : '/';
+    var r = new Request('https://ncmapis.dsfdsfwxp.workers.dev' + u + ur.search,o);
 
     return await worker.fetch(r)
 }
